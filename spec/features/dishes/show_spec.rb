e04cf4bfc9dd86c407f9db777a7f26348829ffba
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-15 11:16:38 -0400

    test: us2 add ing to dish

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,26 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe "Dish show page", type: :feature do
+  before(:each) do 
+    @chef = Chef.create!(name: "Gordon Ramsay")
+    @dish = @chef.dishes.create!(name: "Beef Wellington", description: "A delicious classic")
+    @ingredient1 = @dish.ingredients.create!(name: "Beef tenderloin", calories: 500)
+    @ingredient2 = @dish.ingredients.create!(name: "Puff pastry", calories: 250)
+    @ingredient3 = @dish.ingredients.create!(name: "Egg yolk", calories: 50)
+    @ingredient4 = Ingredient.create!(name: "Egg white", calories: 20)
+  end
+
   it "displays the dish's name, description, ingredients, total calorie count, and chef's name" do
-    chef = Chef.create!(name: "Gordon Ramsay")
-    dish = chef.dishes.create!(name: "Beef Wellington", description: "A delicious classic")
-    ingredient1 = dish.ingredients.create!(name: "Beef tenderloin", calories: 500)
-    ingredient2 = dish.ingredients.create!(name: "Puff pastry", calories: 250)
-    ingredient3 = dish.ingredients.create!(name: "Egg yolk", calories: 50)
+    visit dish_path(@dish)
 
-    visit dish_path(dish)
-    save_and_open_page
-
-    expect(page).to have_content(dish.name)
-    expect(page).to have_content(dish.description)
+    expect(page).to have_content(@dish.name)
+    expect(page).to have_content(@dish.description)
     
     within(".ingredients") do
-      expect(page).to have_content(ingredient1.name)
-      expect(page).to have_content(ingredient2.name)
-      expect(page).to have_content(ingredient3.name)
+      expect(page).to have_content(@ingredient1.name)
+      expect(page).to have_content(@ingredient2.name)
+      expect(page).to have_content(@ingredient3.name)
     end
     
-    expect(page).to have_content("Total calories: #{dish.total_calories}")
-    expect(page).to have_content("Chef: #{chef.name}")
+    expect(page).to have_content("Total calories: #{@dish.total_calories}")
+    expect(page).to have_content("Chef: #{@chef.name}")
+  end
+
+  it 'adds an existing ingredient' do
+    visit dish_path(@dish)
+    save_and_open_page
+
+    expect(page).not_to have_content(@ingredient4.name)
+
+    within("#add-ing-form") do
+      fill_in "ingredient_id", with: @ingredient4.id
+      click_button "Submit"
+    end
+    save_and_open_page
+    expect(current_path).to eq(dish_path(@dish))
+    expect(page).to have_content(@ingredient4.name)
   end
 end
