a97b8daf83d31edea8eae0fccf29e1976542b53d
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-15 11:17:03 -0400

    routes: us2, us3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :dishes, only: [:show]
+  resources :dishes, only: [:show] do
+    resources :ingredients, only: [:create], controller: 'dishes_ingredients'
+  end
+
+  resources :chefs, only: [:show] do
+    resources :ingredients, only: [:index], controller: 'chefs_ingredients'
+  end
 end
